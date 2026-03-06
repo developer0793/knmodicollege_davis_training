ba67d8d383791d4dd82501ce3b2cee9db411909a
developer0793 <aurixisintellix@gmail.com>
2026-03-06 10:52:36 +0530

    basic programs in python

---
+++ pythonbasics/numriccalculation.py
@@ -0,0 +1,23 @@
+#program to perform numriccalculation
+#creating variables to store numbers
+num1=10 
+num2=5
+#------------------------------------------
+#performing addition
+print("The addition of",num1,"and",num2,"is",(num1+num2))
+#performing subtraction
+print("The Difference of",num1,"and",num2,"is",(num1-num2))
+#performing multiplication
+print("The Product of",num1,"and",num2,"is",(num1*num2))
+#performing division                    
+print("The Quotient of",num1,"and",num2,"is",(num1/num2))
+#performing modulus
+print("The Remainder of",num1,"and",num2,"is",(num1%num2))  
+#-------------------------------------------
+'''Output:  
+The addition of 10 and 5 is 15
+The Difference of 10 and 5 is 5
+The Product of 10 and 5 is 50
+The Quotient of 10 and 5 is 2.0
+The Remainder of 10 and 5 is 0
+'''
