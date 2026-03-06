86cd6e232886a1a2682c794d2507841cead1fa6f
developer0793 <aurixisintellix@gmail.com>
2026-03-06 11:17:16 +0530

    Program to perform operations on circle

---
+++ pythonbasics/circleoperation.py
@@ -0,0 +1,17 @@
+#program to find out area and perimeter of the circle
+#creating variable to store radius of the circle
+radius=5
+#displaying radius of the circle
+print("----- Circle -----")
+print("Radius : ",radius,"cm")
+#Displaying perimeter of the circle
+print("Perimeter : ",(3.14*radius*2),"cm")
+#Displaying area of the circle
+print("Area : ",(3.14*radius**2),"sq cm")
+#-------------------------------------------
+'''Output:  
+----- Circle -----
+Radius :  5 cm
+Perimeter :  31.400000000000002 cm
+Area :  78.5 sq cm
+'''
