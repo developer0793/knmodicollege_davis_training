6b0dbd961c43c2866a13ef3d47da7e46487603bb
developer0793 <aurixisintellix@gmail.com>
2026-03-07 10:27:29 +0530

    Program to check a number is natural number or not

---
+++ selection statements/naturalnumbercheck.py
@@ -0,0 +1,17 @@
+#program to check a number is natural number or not
+#taking input from user
+num=int(input("Enter a number : ")) 
+#checking the number is natural number or not
+if num>0:
+    print(num," is a natural number")
+else:
+    print(num," is not a natural number")
+#----------------------------------------------------------
+'''Output:
+Enter a number : 5
+5  is a natural number
+----------------------------------------------------------
+Output 2:
+Enter a number : -3
+-3  is not a natural number
+'''
